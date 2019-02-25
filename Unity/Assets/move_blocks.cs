d4d18cc8ffc6f3d4fcadd6642987d02ae649623e
ttk2563b <t42683384@gmail.com>
2019-02-25 20:54:47 +0900

    コメントなし、後で追加

---
+++ Unity/Assets/move_blocks.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+public class move_blocks : MonoBehaviour
+{
+
+    private bool selected;
+
+    void Update()
+    {
+        if (selected == true)
+        {
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(cursorPos.x, cursorPos.y);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selected = false;
+        }
+    }
+    void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            selected = true;
+        }
+    }
+
+
+}
+ 
+
