4662c8b7db2229324c6d484e9fcf2866f2f680f9
ttk2563b <t42683384@gmail.com>
2019-02-26 21:49:01 +0900

    更新2

--- Unity/Assets/move_blocks.cs
+++ Unity/Assets/move_blocks.cs
@@ -3,26 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+//更新履歴
+// ver     内容
+// 1.0　　マウスクリックでブロックを移動させる
 
-
+//  >> Update Start ver1.0
 public class move_blocks : MonoBehaviour
 {
 
     private bool selected;
 
+
     void Update()
     {
+        //マウスがクリックされている間、座標を更新し続ける
         if (selected == true)
         {
             Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector2(cursorPos.x, cursorPos.y);
         }
 
+        //クリックされていない場合、座標の更新を行わない
         if (Input.GetMouseButtonUp(0))
         {
             selected = false;
         }
     }
+
+    //マウスクリックされた場合
     void OnMouseOver()
     {
 
@@ -34,5 +42,6 @@ public class move_blocks : MonoBehaviour
 
 
 }
+// << Update End ver1.0
  
 
